bdd73b2e1b8f22c695a95810a75868e851ef66ff
alisdhameliya <alisdhameliya24@gmail.com>
2024-08-01 15:28:15 +0530

    vesion 2 is added.

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,8 @@ import tsconfigPaths from 'vite-tsconfig-paths'
 // https://vitejs.dev/config/
 export default defineConfig({
   build:{
-    outDir:"build"
+    outDir:"build",
+    outDir:"dist"
   },
   plugins: [tsconfigPaths(),react()],
 })
